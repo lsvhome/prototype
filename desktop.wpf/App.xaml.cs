02d3560e8a8183b4c63a7c1a78efc8d23b6af45b
Sergii Lutai <slutay@buildbunker.com>
2017-11-02 18:32:34 +0200

    Tray Icon for WPF

--- desktop.wpf/App.xaml.cs
+++ desktop.wpf/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace desktop.wpf
 {
@@ -15,13 +16,32 @@ namespace desktop.wpf
     {
         public static desktop.common.IIocWrapper container = new desktop.common.IocWrapper();
 
+        private TaskbarIcon notifyIcon;
+
+        public App()
+        {
+            InitContainer();
+        }
+
+        public void InitContainer()
+        {
+            Dictionary<Type, object> mappings = new Dictionary<Type, object>();
+            mappings.Add(typeof(desktop.common.IPlatformServices), new PlatformServicesWPF());
+            container.Init(mappings);
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            Dictionary<Type, object> mappings = new Dictionary<Type, object>();
-            mappings.Add(typeof(desktop.common.IPlatformServices), new PlatformServicesWPF());
-            container.Init(mappings);
+            //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+            notifyIcon = (TaskbarIcon) FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
+            base.OnExit(e);
         }
     }
 }
