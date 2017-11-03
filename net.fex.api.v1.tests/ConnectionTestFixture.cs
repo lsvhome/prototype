9563e8fc5f59d8b6b9bd7bbc979eca7eb409b5b6
Sergii Lutai <slutay@buildbunker.com>
2017-11-03 20:34:07 +0200

    Replace Unity with AutoFac Added api project Added api test project Show tray icon on mac and windows

---
+++ net.fex.api.v1.tests/ConnectionTestFixture.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace net.fex.api.v1.tests
+{
+    [TestClass]
+    public class ConnectionTestFixture
+    {
+        const string loginValid = "slutai";
+        const string passwordValid = "100~`!@#$%^&*()[]{}:;\"',<.>/?+=-_";
+
+        [TestMethod]
+        public async Task SignInSignOuTestOk()
+        {
+            using (var conn = new net.fex.api.v1.Connection(new Uri("https://fex.net")))
+            {
+                var user = await conn.SignInAsync(loginValid, passwordValid, false);
+                Assert.IsNotNull(user);
+                Assert.AreEqual(loginValid, user.Login);
+
+                await conn.SignOutAsync();
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Connection.LoginException))]
+        public async Task SignInFailFakeLogin()
+        {
+            try
+            {
+                using (var conn = new net.fex.api.v1.Connection(new Uri("https://fex.net")))
+                {
+                    await conn.SignInAsync("fakelogin", "fakepassword", false);
+                }
+
+            }
+            catch (Connection.LoginException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Connection.LoginException))]
+        public async Task SignInFailFakePassword()
+        {
+            using (var conn = new net.fex.api.v1.Connection(new Uri("https://fex.net")))
+            {
+                await conn.SignInAsync(loginValid, "fakepassword", false);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Connection.ConnectionException))]
+        public async Task SignInOnFakeUrl()
+        {
+            using (var conn = new net.fex.api.v1.Connection(new Uri("https://fake.net")))
+            {
+                var user = await conn.SignInAsync("fakelogin", "fakepassword", false);
+            }
+        }
+    }
+}
---
+++ net.fex.api.v1/Extenders.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.fex.api.v1
+{
+    public static class Extenders
+    {
+        internal static void Process(this Exception exception)
+        {
+            System.Diagnostics.Debugger.Break();
+        }
+
+        internal static Uri AppendQuery(this Uri uri, string key, string value)
+        {
+            UriBuilder uriBuilder = new UriBuilder(uri);
+            if (string.IsNullOrWhiteSpace(uriBuilder.Query))
+            {
+                uriBuilder.Query = string.Format("{0}={1}",key, System.Net.WebUtility.UrlEncode(value));
+            }
+            else
+            {
+                uriBuilder.Query = uriBuilder.Query.TrimStart('?') + string.Format("&{0}={1}", key, System.Net.WebUtility.UrlEncode(value));
+            }
+
+            return uriBuilder.Uri;
+        }
+    }
+}
