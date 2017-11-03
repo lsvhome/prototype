ef92f183b9be64fc4d6bc4cf816f72103736e3b0
Sergii Lutai <slutay@buildbunker.com>
2017-11-03 19:58:46 +0200

    sdfg

---
+++ aaaa/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace aaaa
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            System.Diagnostics.Debug.WriteLine("aaaa");
+        }
+    }
+}
